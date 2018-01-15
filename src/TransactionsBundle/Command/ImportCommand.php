9d7336369ca9afb9ebc9e9eb686c961aec44571c
ammartins <amanuel.martins@gmail.com>
2018-01-15 07:23:45 +0100

    Adding Parsing for csv files for millBCP

--- src/TransactionsBundle/Command/ImportCommand.php
+++ src/TransactionsBundle/Command/ImportCommand.php
@@ -48,6 +48,10 @@ class ImportCommand extends ContainerAwareCommand
         if ($fileContent) {
             dump('Start importing');
             dump(count($fileContentArray));
+
+            // Identify File type
+            preg_match("/\.csv/", $fileLocation) ? $limit = ";" : $limit = "\t";
+
             foreach ($fileContentArray as $line) {
                 // Clean end of string
                 $line = rtrim($line);
@@ -55,14 +59,12 @@ class ImportCommand extends ContainerAwareCommand
                     continue;
                 }
 
-                $info = explode("\t", $line);
-                $correctDate = substr($info[2],0,4).'-'.substr($info[2],4,2).'-'.substr($info[2],6,2);
-                $Date = new \DateTime($correctDate);
-
                 # Generate Hash
                 $hashString = $line;
                 $hash = hash('md5', $hashString, False);
-                $verify = $em->getRepository('TransactionsBundle:Transactions')->getTransactionByHash($hash);
+                $verify = $em
+                    ->getRepository('TransactionsBundle:Transactions')
+                    ->getTransactionByHash($hash);
 
                 // Check if this is already on DB and if so continue
                 // Should probably clean this a bit
@@ -71,16 +73,41 @@ class ImportCommand extends ContainerAwareCommand
                     continue;
                 }
 
+                $info = explode($limit, $line);
+                if ($limit == ";") {
+                    if (!preg_match('/[a-zA-Z]/',$info[0])) {
+                        $Data = new \DateTime($info[0]);
+                        $description = $info[2];
+                        $value = $info[3];
+                        $saldo = $info[5];
+                    }
+                    continue;
+                }
+
+                $correctDate = substr($info[2],0,4).
+                    '-'.substr($info[2],4,2).'-'.
+                    substr($info[2],6,2);
+                $Date = new \DateTime($correctDate);
+
                 $transaction = new Transactions();
 
                 $transaction->setTransactionHash($hash);
                 $transaction->setCreateAt($Date);
                 $transaction->setAmount(
-                  floatval(str_replace(',', '.', str_replace('.', '', $info[6]))));
+                    floatval(
+                        str_replace(',', '.', str_replace('.', '', $info[6]))
+                    )
+                );
                 $transaction->setstartsaldo(
-                  floatval(str_replace(',', '.', str_replace('.', '', $info[3]))));
+                    floatval(
+                        str_replace(',', '.', str_replace('.', '', $info[3]))
+                    )
+                );
                 $transaction->setEndsaldo(
-                  floatval(str_replace(',', '.', str_replace('.', '', $info[4]))));
+                    floatval(
+                        str_replace(',', '.', str_replace('.', '', $info[4]))
+                    )
+                );
                 $transaction->setDescription($info[7]);
                 $transaction->setShortDescription('');
                 $transaction->setAccountId($account);
